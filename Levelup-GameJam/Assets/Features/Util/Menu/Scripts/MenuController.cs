92d678733722614631a473fde91e75ef410faa26
GeovaniPrestes <geovani@sistemasbr.net>
2024-11-02 20:05:10 -0300

    Resolvido problema do Logo do jogo

--- Levelup-GameJam/Assets/Features/Util/Menu/Scripts/MenuController.cs
+++ Levelup-GameJam/Assets/Features/Util/Menu/Scripts/MenuController.cs
@@ -7,6 +7,27 @@ using UnityEngine.SceneManagement;
 public class MenuController : MonoBehaviour
 {
     [SerializeField] private string _sceneName;
+    [SerializeField] public Animator animator;  
+    
+    public string animationTriggerName = "LogoAnimation";  
+    public float minInterval = 5f; 
+    public float maxInterval = 10f; 
+
+    private void Start()
+    {
+        StartCoroutine(PlayAnimationWithRandomInterval());
+    }
+
+    private System.Collections.IEnumerator PlayAnimationWithRandomInterval()
+    {
+        while (true)
+        {
+            float interval = Random.Range(minInterval, maxInterval);
+            Debug.Log(interval);
+            yield return new WaitForSeconds(interval);
+            animator.SetTrigger(animationTriggerName);
+        }
+    }
 
     public void StartGame() =>
         SceneManager.LoadScene(_sceneName);
@@ -21,11 +42,12 @@ public class MenuController : MonoBehaviour
 
     }
 
-    public void QuitGame() {
-        #if UNITY_EDITOR
+    public void QuitGame()
+    {
+#if UNITY_EDITOR
             UnityEditor.EditorApplication.isPlaying = false;
-        #else
-            Application.Quit();
-        #endif
+#else
+        Application.Quit();
+#endif
     }
 }
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # LevelUp-Gamejam
 Repositório para salvar o projeto da Level up Gamejam
+
+![Remember me](https://imgur.com/a/oHNqVtf)
