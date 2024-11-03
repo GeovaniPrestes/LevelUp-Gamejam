4f4adc7ef6576be7c4ed008228e906d51e10a8ba
adrian.quindeler <adrianquindeler@gmail.com>
2024-11-02 21:36:56 -0300

    Player

---
+++ Levelup-GameJam-V1.0/Assets/Features/Util/Scripts/EnemyArea.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+
+public class EnemyArea : MonoBehaviour{
+
+    Player _player;
+
+    void Start(){
+        _player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();   
+    }
+
+    void OnTriggerEnter2D(Collider2D other){
+        if(other.tag == "Player"){
+            StartCoroutine(EnemySpawn());
+        }
+    }
+
+    IEnumerator EnemySpawn(){
+        while(true){
+            yield return new WaitForSeconds(2);
+
+            if(_player.GetMovementDirection() != Vector2.zero){
+                float chance = Random.Range(0f, 1f);
+                if(chance <= 0.30f){
+                    SceneManager.LoadScene("Battle");
+                    yield break;
+                }
+            }
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other){
+        if(other.tag == "Player"){
+            StopAllCoroutines();
+        }
+    }
+}
